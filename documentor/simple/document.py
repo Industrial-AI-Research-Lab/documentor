976a10b08aa1adf9fef213d157ef7e27647245fa
Anatoly <anatoliypershinov@gmail.com>
2024-02-06 18:43:14 +0300

    add from_df and lemmatization methods

--- documentor/simple/document.py
+++ documentor/simple/document.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
-from documentor.abstract.document import Document, Fragment
+from documentor.abstract.document import Document
+from documentor.simple.fragment import SimpleFragment
 
 class SimpleDocument(Document):
     """
@@ -8,11 +9,36 @@ class SimpleDocument(Document):
     :param data: The data from document
     :type data: list[Fragment]
     """
-    data: list[Fragment] = []
+    data: list[SimpleFragment]
 
     def __init__(self):
         ...
 
     @classmethod
-    def from_df(cls, df: pd.DataFrame):
-        ...
+    def from_df(cls, df: pd.DataFrame, target_column: str | None = None,
+                *args, **kwargs) -> 'SimpleDocument':
+        """
+        Validate dataframe and transform it into a simple document
+        :param df: pandas DataFrame with data
+        :type df: pd.DataFrame
+        :param target_column: Column name from which the text will be extracted
+        :type target_column: str
+        :return: SimpleDocument object
+        :rtype: SimpleDocument
+        """
+        if target_column is None:
+            target_column = df.columns[0]
+
+        new_instance = SimpleDocument()
+        new_instance.data = [SimpleFragment(k) for k in df[target_column].values.tolist()]
+
+        return new_instance
+
+    def lemmatize(self, *args, **kwargs):
+        """
+        Lemmatize all fragments in data
+        :return: None
+        :rtype: None
+        """
+        for fragment in self.data:
+            fragment.lemmatization(*args, **kwargs)
