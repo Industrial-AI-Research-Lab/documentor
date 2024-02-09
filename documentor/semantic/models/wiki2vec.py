55c03a1eedba73c950c83073be81863e0fadb1f4
Anatoly <anatoliypershinov@gmail.com>
2024-02-09 17:53:14 +0300

    add semantic models

---
+++ documentor/semantic/models/wiki2vec.py
@@ -0,0 +1,31 @@
+from semantic.models.base import BaseSemanticModel
+import numpy as np
+
+from wikipedia2vec import Wikipedia2Vec
+
+
+class WikiWord2VecModel(BaseSemanticModel):
+    """
+    Represent a wikiword2vec model interface
+    """
+    def __init__(self):
+        super(WikiWord2VecModel, self).__init__()
+
+    def load_weights(self, path, *args, **kwargs):
+        """
+        Load weights from given path and initialize model
+        :param path: path to weights file
+        :type path: str
+        """
+        self.model = Wikipedia2Vec.load(path)
+
+    def encode(self, X, *args, **kwargs) -> np.ndarray:
+        """
+        encode word to vector
+        :param X: word to encode
+        :type X: str
+        :return: vector
+        :rtype: np.ndarray
+        """
+        return self.model.get_word_vector(X)
+
