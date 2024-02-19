cbb735673e22c741122db48bd674efb98c8b9673
Anatoly <anatoliypershinov@gmail.com>
2024-02-19 11:35:05 +0300

    update docs

--- documentor/semantic/models/base.py
+++ documentor/semantic/models/base.py
@@ -1,9 +1,14 @@
 from abc import ABC, abstractmethod
 
+import numpy as np
+
 
 class BaseSemanticModel(ABC):
     """
-    Abstract class for semantic models interface
+    Abstract class for semantic models interface.
+    Text processing models should use this class as parent
+    :param model: Some text processing model
+    :type model: any
     """
     model: any
 
@@ -11,20 +16,21 @@ class BaseSemanticModel(ABC):
         self.model = model
 
     @abstractmethod
-    def load_weights(self, path, *args, **kwargs):
+    def load_weights(self, *args, **kwargs):
         """
-        load weights for model if needed
-        :param path:
-        :return:
+        Method for load weights into model
+        :param args: some args, must be a path or file or any entity
+        :return: None
         """
         ...
 
     @abstractmethod
-    def encode(self, X, *args, **kwargs):
+    def encode(self, X, *args, **kwargs) -> np.ndarray:
         """
         process the data and return it
         :param X: Data for processing
-        :type X: np.Array
-        :return:
+        :type X: any collection words or one words
+        :return: numpy array of tokens
+        :rtype: np.ndarray
         """
         ...
