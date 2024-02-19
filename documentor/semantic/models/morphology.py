93866b86c1e2540e7faeb74c19c80d5d87c4bf97
Anatoly <anatoliypershinov@gmail.com>
2024-02-19 17:39:19 +0300

    move natasha prepocessor to special class

---
+++ documentor/semantic/models/morphology.py
@@ -0,0 +1,45 @@
+import re
+
+import numpy as np
+
+from semantic.models.base import BaseSemanticModel
+
+from natasha import (Segmenter, MorphVocab, Doc, NewsEmbedding, NewsMorphTagger)
+
+
+class NatashaMorphologyModel(BaseSemanticModel):
+    """Class for lemmatize and stemming using natasha"""
+
+    def __init__(self):
+        """
+        Initialize ancillary elements such as segmenter, morphVocab,
+        morph tag and word patternt
+        """
+
+        self.segmenter = Segmenter()
+        self.morph_vocab = MorphVocab()
+        emb = NewsEmbedding()
+        self.morph_tag = NewsMorphTagger(emb)
+        self.word_pattern = re.compile(r'^[a-zA-Zа-яА-ЯёЁ]+$')
+
+    def load_weights(self, path):
+        ...
+
+    def encode(self, X: str, *args, **kwargs) -> tuple:
+        """
+        Method for lemmatize word in statmment X
+        :param X: statement for lemmatize
+        :type X: str
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        doc = Doc(X)
+        doc.segment(self.segmenter)
+        doc.tag_morph(self.morph_tag)
+
+        for token in doc.tokens:
+            token.lemmatize(self.morph_vocab)
+
+        return tuple(word.lemma for word in doc.tokens if self.word_pattern.match(word.lemma))
+
