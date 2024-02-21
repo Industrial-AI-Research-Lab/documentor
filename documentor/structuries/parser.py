0fb98a5974aa4b8b265e9e6640b823e96d0ac8e3
angrymuskrat <mlhakov2011@gmail.com>
2024-02-21 16:02:39 +0300

    change parser object

--- documentor/structuries/parser.py
+++ documentor/structuries/parser.py
@@ -1,6 +1,8 @@
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
 
+import pandas as pd
+
 from documentor.structuries.document import Document
 
 
@@ -11,49 +13,20 @@ class ExtensionException(Exception):
     pass
 
 
-class SimpleParser(ABC):
+class DocumentParser(ABC):
     """
     An abstract class for primary document processing.
     """
     @abstractmethod
-    def from_file(self, path: str) -> Document:
+    def parse_file(self, path: str) -> pd.DataFrame:
         """
-        Create Document from file.
+        Parse file and pandas DataFrame with data about fragments.
 
         :param path: path to file
         :type path: str
         :return: Document object
         :rtype: Document
         :raises ExtensionException: if file extension is not supported
-        :raises DocumentParsingException: if file structure is not valid
         :raises OSError: if file is not found or can't be opened
-        """
-        pass
-
-    @abstractmethod
-    def from_csv(self, path: str, sep: str | None) -> Document:
-        """
-        Create Document from pandas DataFrame.
-
-        :param path: path to file
-        :param sep: separator for csv file
-        :return: Document object
-        :rtype: Document
-        :raises DocumentParsingException: if csv structure is not valid
-        :raises OSError: if file is not found or can't be opened
-        """
-        pass
-
-    @abstractmethod
-    def to_csv(self, document: Document, sep: str | None):
-        """
-        Save Document to csv file.
-
-        :param document: Document object for storing in csv file
-        :type document: Document
-        :param sep: separator for csv file
-        :type sep: str
-        :return:
-        :raises OSError: if document can't be written to file
         """
         pass
